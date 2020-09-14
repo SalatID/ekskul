d8c44b7d99165d51478de07e11b119f2ac0a4e3e
SalatID <31203124+SalatID@users.noreply.github.com>
2020-09-14 13:34:44 +0700

    Update User.php

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
 {
     use Notifiable;
     protected $table = 'tb_user';
-    public $timestamps = false;
+    public $timestamps = false; 
     /**
      * The attributes that are mass assignable.
      *
